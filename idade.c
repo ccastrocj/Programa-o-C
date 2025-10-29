1b9b3b663224903f865b210c2da6cc4e46fa36de
Cristiano de Castro <cristianojunior0809@gmail.com>
2025-10-29 00:15:02 +0000

    commit

---
+++ idade.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+
+    int idade1 = 30;
+    int idade2 = 30;
+
+    if (idade1 != idade2) {
+        printf("As pessoas tem idade diferente\n");
+    }
+
+
+}
---
+++ logica.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+
+    int numero1, numero2;
+
+    numero1 = 2;
+    numero2 = 5;
+    
+    if (numero1 > numero2) {
+
+    printf("Numero 1 é maior que Numero 2.");
+
+    }
+
+    printf("Fora IF");
+
+}
